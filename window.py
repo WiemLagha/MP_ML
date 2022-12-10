101e8e74102163427eeb9f8c1f927c415cad15e9
WiemLagha <laghawiem98@gmail.com>
2022-12-10 01:04:53 +0100

    final version

--- window.py
+++ window.py
@@ -1,7 +1,10 @@
+from asyncio import coroutines
 import tkinter as tk
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 from PIL import Image, ImageTk
+
+from pretty_confusion_matrix import pp_matrix_from_data
 
 from tensorflow.python import keras
 from keras.preprocessing import image
@@ -41,18 +44,27 @@ def trainig(filename):
     ])
 
     model.compile(loss='binary_crossentropy', optimizer= RMSprop(lr=0.01), metrics=['accuracy'])
-    model_fit= model.fit(train_dataset, steps_per_epoch=3, epochs=1, validation_data=test_dataset)
+    model_fit= model.fit(train_dataset, steps_per_epoch=3, epochs=5, validation_data=test_dataset)
     img= load_img(filename,target_size = (200,200))
     X= img_to_array(img)
     X=np.expand_dims(X,axis=0)
     images= np.vstack([X])
     val= model.predict(images)
     if (val==0):
-        print ('normal \n')
+        message="Normal"
     elif (val == 1 ):
-        print('begnin \n')
+        message="Benign"
     else:
-        print('malin \n')
+        message="Malignant"
+
+    tk.messagebox.showinfo(title="Classe", message=message)
+    fig = plt.figure()
+    plt.plot(model_fit.history['accuracy'], color='teal', label='accuracy')
+    fig.suptitle('Accuracy', fontsize=20)
+    plt.legend(loc="upper left")
+    plt.show()
+
+
     
 
 
@@ -61,25 +73,25 @@ def upload_file():
     filename=tk.filedialog.askopenfilename(filetypes=f_types)
     img=ImageTk.PhotoImage(file=filename)
     e1=tk.Label(my_w)
-    e1.grid(row=3, column=1)
+    e1.grid(row=4, columnspan=2)
     e1.image=img
     e1['image']=img
-    asyncio.run_coroutine_threadsafe(trainig(filename))
+    asyncio.run_coroutine_threadsafe(loop=trainig(filename))
     
     
     
     
 
 my_w= tk.Tk()
-my_w.geometry('410x300')
+my_w.geometry('600x300')
 my_w.title ("import image")
-my_w.font1=('times',18,'bold')
+# my_w.font=('times',18,'bold')
 
-l1=tk.Label(my_w,text='Upload files', width=50)
-l1.grid(row=1,columnspan=4)
+l1=tk.Label(my_w,text='Upload files', width=50, font=("Arial", 15))
+l1.grid(row=1,columnspan=1)
 
 b1=tk.Button(my_w,text='Upload image', width=20, command=lambda:upload_file())
-b1.grid(row=2, column=1,columnspan=4)
+b1.grid(row=3,columnspan=5)
 my_w.mainloop()
 
 
